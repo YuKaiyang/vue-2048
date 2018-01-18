c8d3020d09488cb1eae203f0c2e669cb9f3aa0fe
YuKaiyang <45544205@qq.com>
2018-01-18 20:21:24 +0800

    左右移动算法(未完成)

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import {check, news} from './math'
+import {check, news, runLeft, runRight} from './math'
 
 Vue.use(Vuex)
 
@@ -10,21 +10,30 @@ const store = new Vuex.Store({
   },
   mutations: {
     start(state) {
-      let count = []
-      while (count.length !== 4) {
-        const randomIndex = Math.floor(Math.random() * 16)
-        if (count.indexOf(randomIndex) === -1) {
-          count.push(randomIndex)
-        }
-      }
-      count.forEach(i => {
-        state.numbers[i] = 2
+      const newNumber = news(state.numbers, 4)
+      newNumber.forEach(i => {
+        Vue.set(state.numbers, i, 2)
       })
     },
     moveLeft(state) {
+      const lineOne = runLeft(state.numbers.slice(0, 4))
+      const lineTwo = runLeft(state.numbers.slice(4, 8))
+      const lineThree = runLeft(state.numbers.slice(8, 12))
+      const linefour = runLeft(state.numbers.slice(12, 16))
+      const newNumber = [...lineOne, ...lineTwo, ...lineThree, ...linefour]
+      newNumber.forEach((v, i) => {
+        Vue.set(state.numbers, i, v)
+      })
     },
     moveRight(state) {
-
+      const lineOne = runRight(state.numbers.slice(0, 4))
+      const lineTwo = runRight(state.numbers.slice(4, 8))
+      const lineThree = runRight(state.numbers.slice(8, 12))
+      const linefour = runRight(state.numbers.slice(12, 16))
+      const newNumber = [...lineOne, ...lineTwo, ...lineThree, ...linefour]
+      newNumber.forEach((v, i) => {
+        Vue.set(state.numbers, i, v)
+      })
     },
     moveUp(state) {
 
--- src/store/math.js
+++ src/store/math.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+
 //计算剩余空格数量
 export const check = (arr) => {
   let res = 0
@@ -21,4 +21,49 @@ export const news = (arr, number) => {
     }
   }
   return res
+}
+//向左移动数字
+export const runLeft = (arr) => {
+  // for (let i = arr.length - 1; i > 0; i--) {
+  //   if (arr[i] !== 0 && arr[i] === arr[i - 1]) {
+  //     arr[i - 1] = arr[i] * 2
+  //     arr[i] = 0
+  //   } else if (arr[i - 1] === 0) {
+  //     arr[i - 1] = arr[i]
+  //     arr[i] = 0
+  //   }
+  // }
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] === 0) {
+      arr[i] = arr[i + 1]
+      arr[i + 1] = 0
+      continue
+    } else if (arr[i] === arr[i + 1]) {
+      arr[i] = arr[i] * 2
+      arr[i + 1] = 0
+    }
+    // if (arr[i] !== 0 && arr[i] === arr[i + 1]) {
+    //   arr[i] = arr[i] * 2
+    //   arr[i + 1] = 0
+    // } else if (arr[i] === 0 && arr[i + 1] !== 0) {
+    //   arr[i] = arr[i + 1]
+    //   arr[i + 1] = 0
+    // } else {
+    //   arr[i] === arr[i + 1]
+    // }
+  }
+  return [...arr]
+}
+//向右移动数字
+export const runRight = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0 && arr[i] === arr[i + 1]) {
+      arr[i + 1] = arr[i] * 2
+      arr[i] = 0
+    } else if (arr[i + 1] === 0) {
+      arr[i + 1] = arr[i]
+      arr[i] = 0
+    }
+  }
+  return [...arr]
 }
