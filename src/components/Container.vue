2fc69c130fad3786075bf922c7180252eb22a1a2
YuKaiyang <45544205@qq.com>
2018-01-18 13:50:06 +0800

    完成界面布局

---
+++ src/components/Container.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <Node 
+    v-for="node in nodes" 
+    :key="node" 
+    :index="node"
+    :number="node"
+    />
+  </div>
+</template>
+
+<script>
+import Node from "./Node.vue";
+
+const nodes = [];
+
+for (let i = 1; i < 17; i++) {
+  nodes.push(i);
+}
+
+export default {
+  components: {
+    Node
+  },
+  data() {
+    return {
+      nodes
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 440px;
+  margin: 0px auto;
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
---
+++ src/components/Node.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="index">
+    {{ number }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    number: {
+      type: Number,
+      required: true
+    },
+    index: {
+      type: Number,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+  height: 110px;
+  width: 110px;
+  box-sizing: border-box;
+  border: 3px solid black;
+  background-color: aqua;
+  font: 3em sans-serif;
+  text-align: center;
+  line-height: 110px;
+  margin-right: -3px;
+  margin-bottom: -3px;
+}
+</style>
