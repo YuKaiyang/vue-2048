db486eec3ea897fdb27eb06a19eb05ce506a6098
YuKaiyang <45544205@qq.com>
2018-01-19 10:02:41 +0800

    添加颜色样式

--- src/components/Node.vue
+++ src/components/Node.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div :class="classObject">
     {{ number }}
   </div>
 </template>
@@ -11,6 +11,23 @@ export default {
       type: [String, Number],
       required: true
     }
+  },
+  computed: {
+    classObject() {
+      return {
+        l2: this.number === 2,
+        l4: this.number === 4,
+        l8: this.number === 8,
+        l16: this.number === 16,
+        l32: this.number === 32,
+        l64: this.number === 64,
+        l128: this.number === 128,
+        l256: this.number === 256,
+        l512: this.number === 512,
+        l1024: this.number === 1024,
+        l2048: this.number === 2048
+      };
+    }
   }
 };
 </script>
@@ -21,11 +38,44 @@ div {
   width: 110px;
   box-sizing: border-box;
   border: 3px solid black;
-  background-color: aqua;
+  background-color: rgba(238, 228, 218, 0.35);
   font: 3em sans-serif;
   text-align: center;
   line-height: 110px;
   margin-right: -3px;
   margin-bottom: -3px;
+}
+.l2 {
+  background-color: #eee4da;
+}
+.l4 {
+  background-color: #ede0c8;
+}
+.l8 {
+  background-color: #f2b179;
+}
+.l16 {
+  background-color: #f59563;
+}
+.l32 {
+  background-color: #f67c5f;
+}
+.l64 {
+  background-color: #24d390;
+}
+.l128 {
+  background-color: #edcf72;
+}
+.l256 {
+  background-color: #391eaf;
+}
+.l512 {
+  background-color: #db2da1;
+}
+.l1024 {
+  background-color: #dd3d15;
+}
+.l2048 {
+  background-color: #d19018;
 }
 </style>
